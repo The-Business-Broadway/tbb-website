d1ec0cfe013d60717d15ef0989739b86f3b3fc3c
Babar Javaid <javaidbabar@gmail.com>
2024-08-25 19:23:08 +0500

    added hero and footer components

---
+++ app/components/Hero.tsx
@@ -0,0 +1,94 @@
+export const Hero = () => {
+  return (
+    <div
+      className="hero min-h-screen"
+      //   style={{
+      //     backgroundImage:
+      //       "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+      //   }}
+    >
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item w-full">
+          <img
+            alt="carousel"
+            src="https://cnc_backend.oto.biz/public/396129_Group-121196%201.webp"
+            className="w-full"
+          />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item w-full">
+          <img
+            alt="carousel"
+            src="https://cnc_backend.oto.biz/public/940402_banner_slideshow_2.webp"
+            className="w-full"
+          />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item w-full">
+          <img
+            alt="carousel"
+            src="https://cnc_backend.oto.biz/public/566365_banner_slideshow_3.webp"
+            className="w-full"
+          />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide4" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item w-full">
+          <img
+            alt="carousel"
+            src="https://cnc_backend.oto.biz/public/934256_banner_slideshow_5.webp"
+            className="w-full"
+          />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        {/* <!-- Slider indicators --> */}
+    <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+        <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
+        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
+    </div>
+      </div>
+      {/* <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div> */}
+    </div>
+  );
+};
